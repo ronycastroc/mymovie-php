d00bea6d3117ccc8de453159eecd57146a44439e
ronycastroc <castroroony@gmail.com>
2023-09-06 13:05:59 -0300

    style: nav header, body and footer

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
   include_once("templates/header.php");
 
 ?>
-  <h1>BODY</h1>
-  <i class="far fa-plus-square"></i>
+  <div id="main-container" class="container-fluid">
+    <h1>BODY</h1>
+    <i class="far fa-plus-square"></i>
+  </div>  
 <?php
   include_once("templates/footer.php");
 ?>
