e03d702a601705fe3e5e9572b7f649ceee95836c
ronycastroc <castroroony@gmail.com>
2023-09-08 13:59:33 -0300

    feat: create user in db

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -1,15 +1,18 @@
 <?php 
 
   require_once("models/User.php");
+  require_once("models/Message.php");
 
   class UserDAO implements UserDaoInterface {
 
     private $conn;
     private $url;
+    private $message;
 
     public function __construct(PDO $conn, $url) {
       $this->conn = $conn;
       $this->url = $url;
+      $this->message = new Message($url);
     }
 
     public function buildUser($data) {
@@ -29,10 +32,41 @@
 
     }
 
-    public function create(User $user, $authUser = false) {}
+    public function create(User $user, $authUser = false) {
+
+      $stmt = $this->conn->prepare("INSERT INTO users(
+          name, lastname, email, password, token
+        ) VALUES (
+          :name, :lastname, :email, :password, :token
+        )");
+
+      $stmt->bindParam(":name", $user->name);
+      $stmt->bindParam(":lastname", $user->lastname);
+      $stmt->bindParam(":email", $user->email);
+      $stmt->bindParam(":password", $user->password);
+      $stmt->bindParam(":token", $user->token);
+
+      $stmt->execute();
+
+      if($authUser) {
+        $this->setTokenToSession($user->token);
+      }
+    
+    }
+
     public function update(User $user, $redirect = true) {}
     public function verifyToken($protected = false) {}
-    public function setTokenToSession($token, $redirect = true) {}
+
+    public function setTokenToSession($token, $redirect = true) {
+      
+      $_SESSION["token"] = $token;
+
+      if($redirect) {
+        $this->message->setMessage("Welcome!", "success", "/editprofile.php");
+      }
+
+    }
+
     public function authenticateUser($email, $password) {}
 
     public function findByEmail($email) {
---
+++ editprofile.php
@@ -0,0 +1,10 @@
+<?php
+  include_once("templates/header.php");
+?>
+  <div id="main-container" class="container-fluid">
+    <h1>Edit Profile</h1>
+    <i class="far fa-plus-square"></i>
+  </div>  
+<?php
+  include_once("templates/footer.php");
+?>
