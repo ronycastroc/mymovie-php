d84fbbba261e5f8cb863468e5dd57ba75e8f9645
ronycastroc <castroroony@gmail.com>
2023-09-08 12:05:23 -0300

    feat: user model and user DAO

--- models/User.php
+++ models/User.php
@@ -1,1 +1,32 @@
-<?php ?>
+<?php 
+
+  class User {
+
+    public $id;
+    public $name;
+    public $lastname;
+    public $email;
+    public $password;
+    public $image;
+    public $bio;
+    public $token;
+    
+  }
+
+  interface UserDaoInterface {
+
+    public function buildUser($data);
+    public function create(User $user, $authUser = false);
+    public function update(User $user, $redirect = true);
+    public function verifyToken($protected = false);
+    public function setTokenToSession($token, $redirect = true);
+    public function authenticateUser($email, $password);
+    public function findByEmail($email);
+    public function findById($id);
+    public function findByToken($token);
+    public function destroyToken();
+    public function changePassword(User $user);
+    
+  }
+
+?>
